56b81912bc7734af0ae8762d1116801c64cc819b
amirmahdi <=>
2021-11-18 00:01:40 -0800

    add file

---
+++ src/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+char *decodeText(char *message)
+{
+	int i;
+	char ch;
+	int key = 3;
+	for (i = 0; message[i] != '\0'; ++i)
+	{
+		ch = message[i];
+		if (ch >= 'a' && ch <= 'z')
+		{
+			ch = ch - key;
+			if (ch < 'a')
+			{
+				ch = ch + 'z' - 'a' + 1;
+			}
+			message[i] = ch;
+		}
+		else if (ch >= 'A' && ch <= 'Z')
+		{
+			ch = ch - key;
+			if (ch < 'A')
+			{
+				ch = ch + 'Z' - 'A' + 1;
+			}
+			message[i] = ch;
+		}
+	}
+	return message;
+	
+}
+int main()
+{
+	char message[100], ch;
+	int i, key;
+	printf("Enter a message to decrypt: ");
+	gets(message);
+	printf("%s", decodeText(message));
+	printf("\n");
+	return 0;
+}
