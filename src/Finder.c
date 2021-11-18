fe9d298f55e4ef818e630efe91912bf5ffa938d4
majiddrn <dareinimajid34@gmail.com>
2021-11-19 00:42:25 +0330

    struct Finder provided in a .h file

--- src/Finder.c
+++ src/Finder.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-// "Finder" is used to store bounds and the string which is found
-// from the bounds.
-struct Finder
-{
-    int start;
-    int end;
-    char str[500];
-};
+#include "FinderObj.h"
 
 void findSubStr(char[], int, int, char[]);
 
---
+++ src/FinderObj.h
@@ -0,0 +1,8 @@
+// "Finder" is used to store bounds and the string which is found
+// from the bounds.
+struct Finder
+{
+    int start;
+    int end;
+    char str[500];
+};
