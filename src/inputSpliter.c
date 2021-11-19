ba2f5e5773c23b86af402d6344214fef6c5e2618
amirmahdi <=>
2021-11-19 09:40:35 -0800

    add input spliter

---
+++ src/inputSpliter.c
@@ -0,0 +1,58 @@
+// A C/C++ program for splitting a string
+// using strtok()
+#include <stdio.h>
+#include <string.h>
+char** splitInput(char* s,char* deli){
+    int end1=0,end2=0;
+    int count = 0;
+    char **strArr=(char**)malloc(sizeof(char*)*3);
+    for(int i=0;i<3;i++)
+    {
+        strArr[i]=(char*)malloc(200);
+    }
+    for(int i = 0;i<strlen(s);i++){
+        if(s[i]=='#'&& s[i+1]=='#'&&s[i+2]=='#'){
+            if (count==0)
+            {
+                end1 = i;
+                count++;
+            }
+            else if (count==1)
+            {
+                end2 = i;
+                break;
+            }
+            
+        }
+    }
+    printf("end 1 = %d\n", end1);
+    printf("end 2 = %d\n", end2);
+    for (int i = 0; i < end1; i++)
+    {
+        strArr[0][i] = s[i];
+
+    }
+    for (int i = end1+3; i < end2; i++)
+    {
+        strArr[1][i] = s[i];
+    }for (int i = end2+3; i < strlen(s); i++)
+    {
+        
+        strArr[2][i] = s[i];
+    }
+    return strArr;
+}
+int main()
+{
+	char str[] = "Geeks###for###Geeks";
+    char** arr=NULL;
+    char deli[] = "###";
+    arr = splitInput(str,deli);
+    printf("%s\n", arr[0]);
+    printf("%s\n", arr[1]);
+    printf("%s\n", arr[2]);
+
+    return 0;
+
+}
+
--- src/processesFucntions/decoder.c
+++ src/processesFucntions/decoder.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include "decoder.h"
-
+#include "string.h"
+char *remove_white_spaces(char *str)
+{
+	int i = 0, j = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ')
+          str[j++] = str[i];
+		i++;
+	}
+	str[j] = '\0';
+	return str;
+}
 char *decodeText(char *message)
 {
 	int i;
 	char ch;
 	int key = 3;
+	strcpy(message,remove_white_spaces(message));
+	
 	for (i = 0; message[i] != '\0'; ++i)
 	{
 		ch = message[i];
@@ -31,13 +45,14 @@ char *decodeText(char *message)
 	return message;
 	
 }
-// int main()
-// {
-// 	char message[100], ch;
-// 	int i, key;
-// 	printf("Enter a message to decrypt: ");
-// 	gets(message);
-// 	printf("%s", decodeText(message));
-// 	printf("\n");
-// 	return 0;
-// }
+
+ int main()
+ {
+ 	char message[100], ch;
+	int i, key;
+ 	printf("Enter a message to decrypt: ");
+ 	gets(message);
+ 	printf("%s", decodeText(message));
+ 	printf("\n");
+ 	return 0;
+ }
