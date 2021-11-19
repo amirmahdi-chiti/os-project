ba38e5e15115e18a5605d47e10a87f7ee46d5f0e
majiddrn <dareinimajid34@gmail.com>
2021-11-19 17:00:55 +0330

    Comments added to Parent.c

--- src/Parent.c
+++ src/Parent.c
@@ -7,29 +7,42 @@
 
 int main() {
     pid_t decoder, finder, placer;
+    // Adresses of fifo files
     char fifos[3][100] = {"../pipes/decoderFifo.txt",
                         "../pipes/finderFifo.txt",
                         "../pipes/placer.Fifo.txt" };
 
+    /* 
+        We have 3 processes and to satisfy this assumption we should
+    do something not to have 8 processes with 3 forks, so we define 3
+    conditions as below to be sure that we do not duplicate the things 
+    we suppose to do.
+        The conditions check that the fifo related file of each
+    process is made or not, it made, it won't fork again and if
+    the related file doesn't exist then forks.
+    */
     if (!access(fifos[0], F_OK)) {
         mkfifo(fifos[0], 0666);
         decoder = fork();
     }
     if (!access(fifos[1], F_OK)) {
-         mkfifo(fifos[1], 0666);
+        mkfifo(fifos[1], 0666);
         finder = fork();
     }
     if (!access(fifos[2], F_OK)) {
-         mkfifo(fifos[2], 0666);
+        mkfifo(fifos[2], 0666);
         placer = fork();
     }
 
+    // It means that we are in the child process of decoder
     if (decoder == 0) {
         
     }
+    // It means that we are in the child process of finder 
     if (finder == 0) {
 
     }
+    // It means that we are in the child process of placer
     if (placer == 0) {
 
     }
